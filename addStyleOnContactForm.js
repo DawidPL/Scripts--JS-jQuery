510c3453b95ce2d41678c4fbd6b6acff52aa0a4a
Dawid H <32899009+DawidPL@users.noreply.github.com>
2019-07-28 22:14:15 +0200

    replace all function() with arrow functions

--- addStyleOnContactForm.js
+++ addStyleOnContactForm.js
@@ -1,20 +1,20 @@
   /*
 
-Script was written for Contact Form. Displays text if you do not check the required checkboxes. 
+Script was written for Wordpress Contact Form. Displays text if you do not check the required checkboxes. 
 It works only for first click. After that new element won't show untill page reload (it can be change by switch '.one' to '.on')
 
 elementToClick - element of the page which after clicking will add a new text (in this example it's <p> element after 'submit' input) 
 
   */
 
-$(document).on('ready', function(){
+$(document).on('ready', () => {
 	let elementClicked;
-	$('.elementToClick').on('click', function(){
+	$('.elementToClick').on('click', () => {
 		elementClicked = true;
     });
     $('input[type="submit"]').removeAttr("disabled");
     if ($('input[type="checkbox"]').prop('checked') === false){
-        $('input[type="submit"]').one('click', function(){
+        $('input[type="submit"]').one('click', () => {
             if(elementClicked !== true){
 				$( 'input[type="submit"]').after( "<p style=''>Text</p>" );
             }
